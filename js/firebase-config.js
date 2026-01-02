cb1e094c19efeace3a8478a96dd46b8a7a12446a
Abdelmageed Ahmed <117668043+AbdelmageedIzzat@users.noreply.github.com>
2026-01-02 00:22:24 -0800

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,13 @@
+// js/firebase-config.js - إعدادات Firebase
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBYour-Valid-API-Key-Here",
+  authDomain: "your-project-id.firebaseapp.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project-id.appspot.com",
+  messagingSenderId: "123456789012",
+  appId: "1:123456789012:web:abcdef1234567890",
+  measurementId: "G-ABCDEF1234"
+};
+
+// ملاحظة: استبدل هذه القيم بإعدادات مشروع Firebase الخاص بك
