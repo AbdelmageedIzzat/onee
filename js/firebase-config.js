47a9f572f2a0cfd7d9ddd9baff7d5f14720cc4f2
Abdelmageed Ahmed <117668043+AbdelmageedIzzat@users.noreply.github.com>
2026-01-02 06:58:13 -0800

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,13 +1,10 @@
-// js/firebase-config.js - إعدادات Firebase
-
+// js/firebase-config.js - النسخة النهائية
 const firebaseConfig = {
-  apiKey: "AIzaSyBYour-Valid-API-Key-Here",
-  authDomain: "your-project-id.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project-id.appspot.com",
+  apiKey: "AIzaSyBYourActualAPIKeyHere123456",
+  authDomain: "nexus-store.firebaseapp.com",
+  projectId: "nexus-store",
+  storageBucket: "nexus-store.appspot.com",
   messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:abcdef1234567890",
+  appId: "1:123456789012:web:abcdef1234567890abcd",
   measurementId: "G-ABCDEF1234"
 };
-
-// ملاحظة: استبدل هذه القيم بإعدادات مشروع Firebase الخاص بك
