4707c4402e5cb3612c8fd67fa6917d03fc13e18c
Abdelmageed Ahmed <117668043+AbdelmageedIzzat@users.noreply.github.com>
2026-01-03 00:02:22 -0800

    Create categories.ts

---
+++ lib/categories.ts
@@ -0,0 +1,6 @@
+export const categories = [
+  { id: "cosmetics", name: "كوزمتيكس" },
+  { id: "accessories", name: "إكسسوارات" },
+  { id: "clothing", name: "ملابس" },
+  { id: "electronics", name: "إلكترونيات" }
+];
